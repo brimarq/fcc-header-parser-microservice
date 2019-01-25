14a1609b691b11d2d7951a88cb04cbf5fd36f891
Brian Marquis <rbmarquis.dev@gmail.com>
2019-01-25 16:28:01 -0600

    header parser endpoint

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  let ipaddress = req.headers['x-forwarded-for'].split(',')[0], 
+      language = req.headers['accept-language'], 
+      software = req.headers['user-agent'];
+  res.json({"ipaddress": ipaddress, "language": language, "software": software});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
